f71d0090ccfa58d519a0e835ad923ebbfcce2658
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-10-19 13:21:02 +0200

    Proxy scraper

--- get_proxy.py
+++ get_proxy.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import random
 
 
 def headers():
@@ -14,54 +15,35 @@ def headers():
 def scrap_proxy():
     url = 'https://free-proxy-list.net/'
     request = requests.get(url)
-    soup = BeautifulSoup(request.text, 'html.parser')
-
-    tables = soup.find('table', {'class': 'table table-striped table-bordered'})
-    data = pd.DataFrame(columns=['ip',
-                                 'port',
-                                 'code',
-                                 'country',
-                                 'anonymity',
-                                 'google',
-                                 'https',
-                                 'last checked'])
-
-    for row in tables.findAll('tr'):
-        columns = row.find_all('td')
-        if columns != []:
-            ip = columns[0].text.strip()
-            port = columns[1].text.strip()
-            code = columns[2].text.strip()
-            country = columns[3].text.strip()
-            anonymity = columns[4].text.strip()
-            google = columns[5].text.strip()
-            https = columns[6].text.strip()
-            last_checked = columns[7].text.strip()
-
-            data2 = {'ip': ip,
-                     'port': port,
-                     'code': code,
-                     'country': country,
-                     'anonymity': anonymity,
-                     'google': google,
-                     'https': https,
-                     'last checked': last_checked}
-            data3 = pd.DataFrame(data=data2, index=[0])
-
-            data = pd.concat([data, data3], ignore_index=True)
+    proxy_tables = pd.read_html(request.text)
+    proxy_table = pd.DataFrame(data=proxy_tables[0])
+    proxy_table = proxy_table[proxy_table['Anonymity'] == 'elite proxy']
+    proxy_table = proxy_table[['IP Address', 'Port']].reset_index(drop=True).drop_duplicates(subset='IP Address')
+    proxy_table.to_csv('proxy_list.csv', header=False,  index=False, sep=':', mode='w')
 
 
 def proxy_list():
-    temp_list = [
-        '139.162.182.54:49165',
-        '96.126.124.197:81',
-        '80.48.119.28:8080',
-        '198.59.191.234:8080',
-        '71.86.129.131:8080',
-        '185.125.125.157:80'
-    ]
+    temp_list = []
+    with open('proxy_list.csv') as file:
+        for line in file:
+            line = line.strip()
+            temp_list.append(line)
+    # temp_list = [x for x in temp_list if x]
+    temp_list.pop()
     return temp_list
 
 
 def get_one_proxy():
-    pass
+    proxy = proxy_list()
+    random.shuffle(proxy)
+    proxy = proxy[0]
+    return proxy
+
+
+scrap_proxy()
+x = proxy_list()
+for i in x:
+    print(i)
+print(get_one_proxy())
+
+print('hello worlds')
